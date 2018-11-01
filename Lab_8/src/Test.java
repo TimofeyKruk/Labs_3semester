5f59d6c6f2e23f71e766580d0f363ffbaca045eb
TIMOFEYKRUK\Tux <kruktimofey@gmail.com>
2018-11-01 16:40:28 +0300

    Lab_8, deleted useless comments

--- Lab_8/src/Test.java
+++ Lab_8/src/Test.java
@@ -13,39 +13,6 @@ public class Test {
             datFile.appendToFileFromTXTFile("Input.txt");
             datFile.printFile();
 
-//            System.out.println("___What do you want to do?___");
-//            System.out.println("1. Append bill to file.");
-//            System.out.println("2. Print file.");
-//            System.out.println("3. Delete file.");
-//            System.out.println("_____________________________");
-//            System.out.print("Your choice: ");
-//
-//            Scanner input = new Scanner(System.in);
-//            int userChoice = input.nextInt();
-//            System.out.println();
-//
-//            switch (userChoice) {
-//                case 1:
-//                    System.out.println("Enter Utility Bill:  ");
-//                    Scanner in = new Scanner(System.in);
-//                    String s = in.nextLine();
-//                    datFile.appendToFile(new UtilityBill(s));
-//
-//                    System.out.println("After appending, .dat file: ");
-//                    datFile.printFile();
-//                    break;
-//                case 2:
-//                    datFile.printFile();
-//                    break;
-//                case 3:
-//                    datFile.deleteFile();
-//                    System.out.println("File was deleted!");
-//                    break;
-//                default:
-//                    System.out.println("Incorrect input!");
-//                    break;
-//            }
-
             //  MAP DEMONSTRATION
             MapUtility billMap = new MapUtility(datFile);
             billMap.showMap();
